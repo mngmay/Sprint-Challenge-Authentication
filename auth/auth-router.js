9852a8ddabc66682ea4e0aecee72b366e74c4728
May Ng <mngmay@gmail.com>
2019-09-20 08:25:57 -0700

    installed dependencies

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,25 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   // implement registration
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
+
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: "8h"
+  };
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
 
 module.exports = router;

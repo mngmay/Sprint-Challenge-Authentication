7a0a35ecc6be1b08a222b6c770e7e185f141ba9a
May Ng <mngmay@gmail.com>
2019-09-20 08:31:43 -0700

    added secrets and users model files

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,6 +2,10 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+const Users = require("../users/users-model.js");
+const secrets = require("../config/secrets.js");
+const restricted = require("./authenticate-middleware.js");
+
 router.post("/register", (req, res) => {
   // implement registration
 });

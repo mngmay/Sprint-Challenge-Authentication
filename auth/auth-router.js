ac2596f3cb8a6b6495bee25fb46d062e29772c31
May Ng <mngmay@gmail.com>
2019-09-20 08:41:36 -0700

    created middleware

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -4,14 +4,40 @@ const jwt = require("jsonwebtoken");
 
 const Users = require("../users/users-model.js");
 const secrets = require("../config/secrets.js");
-const restricted = require("./authenticate-middleware.js");
 
 router.post("/register", (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 router.post("/login", (req, res) => {
   // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      console.log(user);
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ token: token });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 function generateToken(user) {
